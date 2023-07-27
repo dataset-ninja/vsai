61e98df36aa45f27170e388af780223ff3db1e08
Ars Vladimirov <grokhael@gmail.com>
2023-07-27 13:26:51 +0600

    upd custom data

--- SUMMARY.md
+++ SUMMARY.md
@@ -1,9 +1,7 @@
-**Multi-View Dataset for Vehicle Detection in Complex Scenarios** is a dataset for instance segmentation tasks. It is used in the air detection industry.
+**VSAI: A Multi-View Dataset for Vehicle Detection in Complex Scenarios Using Aerial Images** is a dataset for an object detection task. It is used in the robotics and logistics industries, and vehicle detection domain. 
 
-The dataset consists of 9075 images with 87802 labeled objects belonging to 2 different classes including _small-vehicle_ and _large-vehicle_.
+The dataset consists of 9075 images with 87802 labeled objects belonging to 2 different classes including *small-vehicle* and *large-vehicle*.
 
-Images in the VSAI dataset have pixel-level instance segmentation annotations. Due to the nature of the instance segmentation task, it can be automatically transformed into a semantic segmentation (only one mask for every class) or object detection (bounding boxes for every object) tasks. There are 3703 (41% of the total) unlabeled images (i.e. without annotations). There are 3 splits in the dataset: _test_ (2315 images), _train_ (5240 images), and _val_ (1520 images). The dataset was released in 2022.
+Images in the VSAI dataset have bounding box annotations. There are 3703 (41% of the total) unlabeled images (i.e. without annotations). There are 3 splits in the dataset: *test* (2315 images), *train* (5240 images), and *val* (1520 images). The dataset was released in 2022 by the [National University of Defense Technology, China](https://english.nudt.edu.cn/).
 
-Here are the visualized examples for each of the 2 classes:
-
-[Dataset classes](https://github.com/dataset-ninja/vsai/raw/main/visualizations/horizontal_grid.webm)
+<img src="https://github.com/dataset-ninja/vsai/raw/main/visualizations/poster.png">
--- src/settings.py
+++ src/settings.py
@@ -1,22 +1,37 @@
 from typing import Dict, List, Optional, Union
 
-from dataset_tools.templates import AnnotationType, CVTask, Industry, License
+from dataset_tools.templates import (
+    AnnotationType,
+    Category,
+    CVTask,
+    Domain,
+    Industry,
+    License,
+    Research,
+)
 
 ##################################
 # * Before uploading to instance #
 ##################################
 PROJECT_NAME: str = "VSAI"
-PROJECT_NAME_FULL: str = "Multi-View Dataset for Vehicle Detection in Complex Scenarios"
+PROJECT_NAME_FULL: str = (
+    "VSAI: A Multi-View Dataset for Vehicle Detection in Complex Scenarios Using Aerial Images"
+)
 
 ##################################
 # * After uploading to instance ##
 ##################################
 LICENSE: License = License.CC_BY_NC_SA_4_0()
-INDUSTRIES: List[Industry] = [Industry.AirDetection()]
-CV_TASKS: List[CVTask] = [CVTask.InstanceSegmentation()]
-ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.InstanceSegmentation()]
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Robotics(), Industry.Logistics(), Domain.VehicleDetection()]
+CATEGORY: Category = Category.Aerial(extra=[Category.Robotics(), Category.Surveillance()])
 
-RELEASE_YEAR: int = 2022
+CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
+ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.ObjectDetection()]
+
+RELEASE_DATE: Optional[str] = "2022-05-31"  # e.g. "YYYY-MM-DD"
+if RELEASE_DATE is None:
+    RELEASE_YEAR: int = None
+
 HOMEPAGE_URL: str = "https://www.kaggle.com/datasets/dronevision/vsaiv1"
 # e.g. "https://some.com/dataset/homepage"
 
@@ -37,10 +52,14 @@ DOWNLOAD_ORIGINAL_URL: Optional[
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 # If specific colors for classes are needed, fill this dict (e.g. {"class1": [255, 0, 0], "class2": [0, 255, 0]})
 
-PAPER: Optional[str] = None
-CITATION_URL: Optional[str] = None
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+PAPER: Optional[str] = "https://www.mdpi.com/2504-446X/6/7/161"
+CITATION_URL: Optional[str] = "https://www.kaggle.com/datasets/dronevision/vsaiv1?select=VSAI.TXT"
+AUTHORS: Optional[List[str]] = ["Wang, Jinghao" , "Teng, Xichao" , "Li, Zhang" , "Yu, Qifeng" , "Bian, Yijie" , "Wei, Jiaqi"]
+
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = "National University of Defense Technology, China"
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = "https://english.nudt.edu.cn/"
+
+SLYTAGSPLIT: Optional[Dict[str, List[str]]] = None
 TAGS: List[str] = None
 
 ##################################
@@ -55,10 +74,15 @@ def check_names():
 
 
 def get_settings():
+    if RELEASE_DATE is not None:
+        global RELEASE_YEAR
+        RELEASE_YEAR = int(RELEASE_DATE.split("-")[0])
+
     settings = {
         "project_name": PROJECT_NAME,
         "license": LICENSE,
-        "industries": INDUSTRIES,
+        "applications": APPLICATIONS,
+        "category": CATEGORY,
         "cv_tasks": CV_TASKS,
         "annotation_types": ANNOTATION_TYPES,
         "release_year": RELEASE_YEAR,
@@ -70,13 +94,16 @@ def get_settings():
     if any([field is None for field in settings.values()]):
         raise ValueError("Please fill all fields in settings.py after uploading to instance.")
 
+    settings["release_date"] = RELEASE_DATE
     settings["project_name_full"] = PROJECT_NAME_FULL or PROJECT_NAME
     settings["download_original_url"] = DOWNLOAD_ORIGINAL_URL
     settings["class2color"] = CLASS2COLOR
     settings["paper"] = PAPER
     settings["citation_url"] = CITATION_URL
+    settings["authors"] = AUTHORS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
-    settings["tags"] = TAGS if TAGS is not None else []
+    settings["slytagsplit"] = SLYTAGSPLIT
+    settings["tags"] = TAGS
 
     return settings
